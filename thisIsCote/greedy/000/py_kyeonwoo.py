5b7d81b9b53ead21002f50fa2a8fc80c0406711b
Park747 <qkrrusdn747@korea.ac.kr>
2022-06-03 01:03:19 +0900

    다시풀이

--- thisIsCote/greedy/000/py_kyeonwoo.py
+++ thisIsCote/greedy/000/py_kyeonwoo.py
@@ -1,58 +1,67 @@
+# N = int(input()) #처음 풀었을때
+# answer = 0
+# count = 0
+# if N%500 == 0:
+#     count += N//500
+# elif N%100 == 0:
+#     while N>0:
+#         N = N-100
+#         count += 1
+#         if N%500 == 0:
+#             count += N//500
+#             break
+# elif N%50 == 0:
+#     while N>0:
+#         N = N-50
+#         count += 1
+#         if N%500 == 0:
+#             count += N//500
+#             break
+#         elif N%100 == 0:
+#             while N>0:
+#                 N = N-100
+#                 count += 1
+#                 if N%500 == 0:
+#                     count += N//500
+#                     break
+#             break
+# elif N%10 == 0:
+#     while N>0:
+#         N = N-10
+#         count += 1
+#         if N%500 == 0:
+#             count += N//500
+#             break
+#         elif N%100 == 0:
+#             while N>0:
+#                 N = N-100
+#                 count += 1
+#                 if N%500 == 0:
+#                     count += N//500
+#                     break
+#             break
+#         elif N%50 == 0:
+#             while N>0:
+#                 N = N-50
+#                 count += 1
+#                 if N%500 == 0:
+#                     count += N//500
+#                     break
+#                 elif N%100 == 0:
+#                     while N>0:
+#                         N = N-100
+#                         count += 1
+#                         if N%500 == 0:
+#                             count += N//500
+#                             break
+#                     break
+# print(count)
+
+#다시푼것
 N = int(input())
-answer = 0
-count = 0
-if N%500 == 0:
-    count += N//500
-elif N%100 == 0:
-    while N>0:
-        N = N-100
-        count += 1
-        if N%500 == 0:
-            count += N//500
-            break
-elif N%50 == 0:
-    while N>0:
-        N = N-50
-        count += 1
-        if N%500 == 0:
-            count += N//500
-            break
-        elif N%100 == 0:
-            while N>0:
-                N = N-100
-                count += 1
-                if N%500 == 0:
-                    count += N//500
-                    break
-            break
-elif N%10 == 0:
-    while N>0:
-        N = N-10
-        count += 1
-        if N%500 == 0:
-            count += N//500
-            break
-        elif N%100 == 0:
-            while N>0:
-                N = N-100
-                count += 1
-                if N%500 == 0:
-                    count += N//500
-                    break
-            break
-        elif N%50 == 0:
-            while N>0:
-                N = N-50
-                count += 1
-                if N%500 == 0:
-                    count += N//500
-                    break
-                elif N%100 == 0:
-                    while N>0:
-                        N = N-100
-                        count += 1
-                        if N%500 == 0:
-                            count += N//500
-                            break
-                    break
-print(count)
+coin_list = [500,100,50,10]
+num = 0
+for i in coin_list:
+    num += N//i
+    N = N%i
+print(num)
